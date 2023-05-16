5a58e802f5660a9886dd7655cadeae59b5a3492f
Jordon de Hoog <jordon.dehoog@gmail.com>
2023-05-16 16:20:06 -0400

    migrate to kotlin build scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,18 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
+    extra["minSdkVersion"] = 21
+    extra["compileSdkVersion"] = 33
+    extra["targetSdkVersion"] = 33
+
     repositories {
         google()
         mavenCentral()
         mavenLocal()
     }
+
     dependencies {
-        classpath 'com.android.tools.build:gradle:8.0.1'
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
+        classpath("com.android.tools.build:gradle:8.0.1")
     }
 }
 
@@ -17,9 +20,10 @@ allprojects {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(buildDir)
 }
---
+++ epf/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    id("com.android.library")
+    id("maven-publish")
+}
+
+android {
+    namespace = "com.daasuu.epf"
+
+    setCompileSdkVersion(property("compileSdkVersion") as Int)
+    defaultConfig {
+        minSdk = property("minSdkVersion") as Int
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    api("androidx.media3:media3-exoplayer:1.0.1")
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "com.daasuu.epf"
+            artifactId = "exoplayerfilter"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
+}
